5e0d43351138f994c8b371fd4a52ff6a1f752c4a
adipurcaru <adipurcaru@gmail.com>
2020-09-14 22:19:18 +0300

    added User entity, repository and service

---
+++ src/main/java/com/ag/rent/domain/User.java
@@ -0,0 +1,151 @@
+package com.ag.rent.domain;
+
+import java.io.Serializable;
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Id;
+
+public class User extends BaseModel<String> implements Serializable{
+
+	/* run this sql to create the table
+	 * 
+	 * create table user(
+		id int not null primary key,
+		username varchar(35) not null,
+		email varchar(150) not null,
+		password varchar(35) not null,
+		birth_date datetime not null,
+		creat_dt datetime,
+		creat_by varchar(50),
+		updt_dt datetime,
+		updt_by varchar(50)
+	)
+	 * 
+	 * */
+	
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 4699009915610641020L;
+
+	@Id
+	@Column(name="id")
+	private Long id;
+	
+	@Column(name="username")
+	private String username;
+	
+	@Column(name="email")
+	private String email;
+	
+	@Column(name="password")
+	private String password;
+	
+	@Column(name="birth_date")
+	private Date dateOfBirth;
+	
+	public User(long id, String username, String email, String password, Date dateOfBirth) {
+		super();
+		this.id = id;
+		this.username = username;
+		this.email = email;
+		this.password = password;
+		this.dateOfBirth = dateOfBirth;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public Date getDateOfBirth() {
+		return dateOfBirth;
+	}
+
+	public void setDateOfBirth(Date dateOfBirth) {
+		this.dateOfBirth = dateOfBirth;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((dateOfBirth == null) ? 0 : dateOfBirth.hashCode());
+		result = prime * result + ((email == null) ? 0 : email.hashCode());
+		result = prime * result + (int) (id ^ (id >>> 32));
+		result = prime * result + ((password == null) ? 0 : password.hashCode());
+		result = prime * result + ((username == null) ? 0 : username.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		User other = (User) obj;
+		if (dateOfBirth == null) {
+			if (other.dateOfBirth != null)
+				return false;
+		} else if (!dateOfBirth.equals(other.dateOfBirth))
+			return false;
+		if (email == null) {
+			if (other.email != null)
+				return false;
+		} else if (!email.equals(other.email))
+			return false;
+		if (id != other.id)
+			return false;
+		if (password == null) {
+			if (other.password != null)
+				return false;
+		} else if (!password.equals(other.password))
+			return false;
+		if (username == null) {
+			if (other.username != null)
+				return false;
+		} else if (!username.equals(other.username))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "User [id=" + id + ", username=" + username + ", email=" + email + ", password=" + password
+				+ ", dateOfBirth=" + dateOfBirth + "]";
+	}
+	
+
+}
---
+++ src/main/java/com/ag/rent/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.ag.rent.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.ag.rent.domain.User;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long>{
+
+}
